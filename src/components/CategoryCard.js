1ffa36e4209e3c384697ef2972ea2cdaf7f8b9f5
Valter Valik <valtervaralik@gmail.com>
2023-04-22 18:15:28 -0400

    editing and deleting categories

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -7,8 +7,38 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import styles from './CategoryCard.module.css';
 import Link from 'next/link';
+import ConfirmDeleteCategory from './ConfirmDeleteCategory';
+import SnackBarContext from '@/context/SnackBarContext';
+import { helpHttp } from '@/helpers/helpHttp';
+import EditCategoryModal from './EditCategoryModal';
 
 export default function CategoryCard({ category }) {
+	const [openDelete, setOpenDelete] = React.useState(false);
+	const handleOpenDelete = () => setOpenDelete(true);
+	const handleCloseDelete = () => setOpenDelete(false);
+
+	const [openEdit, setOpenEdit] = React.useState(false);
+	const handleOpenEdit = () => setOpenEdit(true);
+	const handleCloseEdit = () => setOpenEdit(false);
+
+	const url = `http://127.0.0.1:5000/categories/${category._id}`;
+
+	const { setOpenSuccessSnack, setMsg } = React.useContext(SnackBarContext);
+
+	const deleteCategory = async () => {
+		await helpHttp()
+			.del(url)
+			.then((res) => {
+				setMsg(res.message);
+				setOpenSuccessSnack(true);
+			});
+
+		setTimeout(() => {
+			setOpenSuccessSnack(false);
+			setMsg('');
+		}, 3000);
+	};
+
 	return (
 		<Card
 			id={styles.categorycard}
@@ -32,13 +62,30 @@ export default function CategoryCard({ category }) {
 				</CardContent>
 			</Link>
 			<CardActions className='d-flex justify-content-around'>
-				<Button className='btn text-primary' size='small'>
+				<Button
+					onClick={handleOpenEdit}
+					className='btn text-primary'
+					size='small'>
 					Editar
 				</Button>
-				<Button className='btn text-danger' size='small'>
+				<Button
+					onClick={handleOpenDelete}
+					className='btn text-danger'
+					size='small'>
 					Eliminar
 				</Button>
 			</CardActions>
+			<ConfirmDeleteCategory
+				deleteCategory={deleteCategory}
+				open={openDelete}
+				category={category}
+				handleClose={handleCloseDelete}
+			/>
+			<EditCategoryModal
+				category={category}
+				openCategory={openEdit}
+				handleClose={handleCloseEdit}
+			/>
 		</Card>
 	);
 }
---
+++ src/components/EditCategoryForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import TextInput from './TextInput';
+import { FormError } from './FormError';
+import { useEditForm } from '@/hooks/useEditForm';
+import ModalEditButtons from './ModalEditButtons';
+
+const validateForm = (form) => {
+	let error = {};
+	let regexInput = /^.{1,55}$/;
+
+	if (!form.category.trim()) {
+		error.category = `El campo 'Categoría' es requerido`;
+	} else if (!regexInput.test(form.category.trim())) {
+		error.category = `El campo 'Categoría' debe tener un máximo de 55 caracteres`;
+	}
+
+	return error;
+};
+
+const EditCategoryForm = ({ handleClose, category }) => {
+	const initialForm = {
+		category: `${
+			category.category[0].toUpperCase() + category.category.substring(1)
+		}`,
+	};
+
+	const url = `http://127.0.0.1:5000/categories/${category._id}`;
+
+	const { form, error, handleChange, handleBlur, handleSubmit } = useEditForm(
+		initialForm,
+		validateForm,
+		url,
+		handleClose
+	);
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} action='/admin/inventory/components'>
+				<div className='d-flex flex-column justify-content-center'>
+					<TextInput
+						handleChange={handleChange}
+						handleBlur={handleBlur}
+						label='Nueva Categoría'
+						name='category'
+						width='43ch'
+						value={form.category}
+					/>
+					{error.category && (
+						<FormError className='text-center'>{error.category}</FormError>
+					)}
+				</div>
+				<ModalEditButtons handleClose={handleClose} />
+			</form>
+		</>
+	);
+};
+
+export default EditCategoryForm;
